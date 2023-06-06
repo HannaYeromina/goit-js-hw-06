72d82a6a0a83f7c66f75992e74fe0ea08439f75a
Anna <anna@Annas-MacBook-Air.local>
2023-06-06 09:12:59 -0700

    task-06

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const validationIinput = document.querySelector("input#validation-input");
 
 validationIinput.addEventListener("blur", () => {
-      if (validationIinput.value.length.trim() === Number(validationIinput.dataset.length)) {
+      if (validationIinput.value.trim().length === Number(validationIinput.dataset.length)) {
         validationIinput.classList.remove("invalid");
         validationIinput.classList.add("valid");
      } else {
